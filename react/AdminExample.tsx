6ec20a8dfd1896be6abecd9a19047fa1c01d0b07
Guilherme Rodrigues <gadr90@gmail.com>
2019-03-29 15:39:33 -0300

    Initial commit

---
+++ react/AdminExample.tsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import { FormattedMessage } from 'react-intl'
+
+class AdminExample extends Component {
+  public render() {
+    return (
+      <FormattedMessage id="admin-example.hello-world" />
+    )
+  }
+}
+
+export default AdminExample
