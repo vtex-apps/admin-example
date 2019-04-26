e37d975ecab2cf1bb31e9f7cb1f0c763fa0e844b
ohmyguigs <ohmyguigs@gmail.com>
2019-04-26 11:46:32 -0300

    Add a nasty css so html height can be 100%

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -2,22 +2,22 @@ import React, { Component } from "react";
 import { FormattedMessage } from "react-intl";
 import { Layout, PageBlock, PageHeader } from "vtex.styleguide";
 
+import "./styles.global.css";
+
 class AdminExample extends Component {
   public render() {
     return (
-      <div className="h-100">
-        <Layout
-          pageHeader={
-            <PageHeader
-              title={<FormattedMessage id="admin-example.hello-world" />}
-            />
-          }
-        >
-          <PageBlock>
-            <FormattedMessage id="appframe.navigation.example" />
-          </PageBlock>
-        </Layout>
-      </div>
+      <Layout
+        pageHeader={
+          <PageHeader
+            title={<FormattedMessage id="admin-example.hello-world" />}
+          />
+        }
+      >
+        <PageBlock>
+          <FormattedMessage id="appframe.navigation.example" />
+        </PageBlock>
+      </Layout>
     );
   }
 }
