60a19438d0fc5d7b6e99ad45fbe19628f4824ee4
ohmyguigs <ohmyguigs@gmail.com>
2019-04-26 10:51:10 -0300

    Updates to use admin builder

--- react/AdminExample.tsx
+++ react/AdminExample.tsx
@@ -1,12 +1,25 @@
-import React, { Component } from 'react'
-import { FormattedMessage } from 'react-intl'
+import React, { Component } from "react";
+import { FormattedMessage } from "react-intl";
+import { Layout, PageBlock, PageHeader } from "vtex.styleguide";
 
 class AdminExample extends Component {
   public render() {
     return (
-      <FormattedMessage id="admin-example.hello-world" />
-    )
+      <div className="h-100">
+        <Layout
+          pageHeader={
+            <PageHeader
+              title={<FormattedMessage id="admin-example.hello-world" />}
+            />
+          }
+        >
+          <PageBlock>
+            <FormattedMessage id="appframe.navigation.example" />
+          </PageBlock>
+        </Layout>
+      </div>
+    );
   }
 }
 
-export default AdminExample
+export default AdminExample;
